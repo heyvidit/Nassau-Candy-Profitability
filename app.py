a28344a9c4b0fa27db91ebf4783977825de0069b
Vidit Kapoor <viditkapoor02@gmail.com>
2026-02-16 14:54:51 +0530

    Adjust margin styles in app.py

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ st.markdown("""
     display:flex;
     justify-content:center;
     align-items:center;
-  margin-top:-10px;
+  margin-top:-15px;
     border-bottom:1px solid #222;
-    margin-bottom:25px;
+    margin-bottom:2rem;
 }
 
 .company-logo {
-    max-width:10rem;
+    min-width:10rem;
     height:4rem;
     
 }
