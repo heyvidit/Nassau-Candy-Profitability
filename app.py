44eda601b01444f4c310153c3da0e9756fdd7881
Vidit Kapoor <viditkapoor02@gmail.com>
2026-02-16 14:52:40 +0530

    Fix missing semicolon in CSS style

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown("""
     display:flex;
     justify-content:center;
     align-items:center;
-  margin-top:2px
+  margin-top:2px;
     border-bottom:1px solid #222;
     margin-bottom:25px;
 }
